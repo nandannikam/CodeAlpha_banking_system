ffbff05944dad51982be78d37781e62c2bdaa5eb
Nandan Nikam <nikam.nandan.35@outlook.com>
2026-05-20 01:11:27 +0530

    Update in banking_system.cpp
    Updated and made functional all the operations that can be performed. Login bugs are solved completely.

--- banking_system.cpp
+++ banking_system.cpp
@@ -3,14 +3,14 @@
 # include <vector>
 using namespace std;
 
-string name;
-int acc_num;
-float amt;
+
 
 class customer {
 
     public :
 
+    string name;
+    int acc_num;
     int p;
 
     void new_acc(){
@@ -29,8 +29,8 @@ class customer {
 
 class account : public customer{
 
-    private :
-    int pin = p;
+    protected :
+    int pin;
     float balance = 0;
 
     public :
@@ -45,33 +45,15 @@ class account : public customer{
         return balance;
     }
 
-    int login(){
+    void set_pin(int x){
+    pin = x;
+}
 
-        int a;
-        int user_pin;
-        int c2;
-
-        cout << "Enter account number - ";
-        cin >> a;
-        cout << "Enter pin - ";
-        cin >> user_pin;
-
-        if (acc_num==a && pin==user_pin){
-            cout << "Logined Succesfully !" << endl;
-            cout << "Menu :" << endl;
-            cout << "1. Deposite Cash" << endl;
-            cout << "2. Withdrawl Cash" << endl;
-            cout << "3. Fund Transfer" << endl;
-            cout << "4. Check Balance" << endl;
-            cout << "5. Exit" << endl;
-            cout << "Enter your choice - " << endl;
-            cin >> c2;
-            return c2;
-        } else {
-            cout << "Invalid Credentials !" << endl;
-            return 0;
+    int login(int a, int user_pin){
+        if (acc_num == a && pin == user_pin){
+            return 1;
         }
-
+        return 0;
     }
 
 
@@ -80,21 +62,7 @@ class account : public customer{
 
 class transaction : public account {
 
-    private :
-
-    account s;
-    int pin;
-    float balance;
-
-
     public :
-
-    void use(){
-        int pin = s.get_pin();
-        float balance = s.get_balance();
-    }
-
-
 
     void withdrawal(float amt){
 
@@ -140,50 +108,101 @@ class transaction : public account {
 
 int main(){
 
-    transaction t1;
-
+    vector<transaction> users;
 
     int c1;
-    int c3 = 0;
+    int c2;
+    float amt;
 
-while (c3!=5) {
+
+
+while (true) {
+
         cout << "Welcome to the Banking System Menu" << endl;
         cout << "1. Create new account" << endl;
         cout << "2. Manage existing account" << endl;
-        cout << "Enter your choice - " << endl;
+        cout << "3. Exit" << endl;
+        cout << "Enter your choice - ";
         cin >> c1;
 
         if(c1 == 1){
-            cout << "Creating new user comming soon" << endl;
+            transaction newUser;
+            newUser.new_acc();
+            newUser.set_pin(newUser.p);
+            users.push_back(newUser);
+            cout << "Account created successfully!" << endl;
 
         } else if (c1==2){
 
-            c3 = t1.login();
+            int a, user_pin;
 
-            if(c3==1){
-                cout << "Enter the amount to be deposited - ";
-                cin >> amt;
-                t1.deposit(amt);
-           
-            } else if (c3==2){
-                cout << "Enter the amount to be withdrawn - ";
-                cin >> amt;
-                t1.withdrawal(amt);
+            cout << "Enter account number - ";
+            cin >> a;
+            cout << "Enter pin - ";
+            cin >> user_pin;
+            
+            int found = -1;
 
-            } else if (c3==3){  
-                cout <<"Fund transfer function comming soon.." << endl;
-            
-            } else if (c3==4){
-                t1.check_balance();
-            
-            } else if (c3==5){
-                
-                cout << "Exiting function comming soon" << endl;
+
+
+            for(int i = 0; i < users.size(); i++){
+                if(users[i].login(a, user_pin)){
+                    found = i;
+                    break;
+                }
             }
 
+            if(found == -1){
+                cout << "No matching account found!" << endl;
+                continue;
+            }
+
+            if(found != -1){
+                cout << "Logged in Succesfully !" << endl;
+
+                while(true){
+
+
+                    cout << "Menu :" << endl;
+                    cout << "1. Deposit Cash" << endl;
+                    cout << "2. Withdrawal Cash" << endl;
+                    cout << "3. Fund Transfer" << endl;
+                    cout << "4. Check Balance" << endl;
+                    cout << "5. Exit" << endl;
+                    cout << "Enter your choice - ";
+                    cin >> c2;
+
+                    if(c2==1){
+                        cout << "Enter the amount to be deposited - ";
+                        cin >> amt;
+                        users[found].deposit(amt);
+
+                    } else if (c2==2){
+                        cout << "Enter the amount to be withdrawn - ";
+                        cin >> amt;
+                        users[found].withdrawal(amt);
+
+                    } else if (c2==3){  
+                        cout <<"Fund transfer function comming soon.." << endl;
+
+                    } else if (c2==4){
+                        users[found].check_balance();
+
+                    } else if (c2==5){
+                        cout << "Exiting..." << endl;
+                        break;
+                    }
+
+                } 
+            }
+
+        } else if (c1==3){
+            cout << "Thank you for using Banking System!" << endl;
+            break;
         } else {
-            cout << "Enter valid input.." << endl;
+            cout << "Enter a valid input" << endl;
         }
+
 
     }
 
